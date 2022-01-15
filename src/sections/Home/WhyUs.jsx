5b1c7556d788ca56c5297039d671c2d2fd83b0a6
Prabodh Panda <46858748+Prabodh-Panda@users.noreply.github.com>
2022-01-15 19:05:08 +0530

    Finished Why Us Section

--- src/sections/Home/WhyUs.jsx
+++ src/sections/Home/WhyUs.jsx
@@ -3,7 +3,7 @@ import WhyUsTile from "../../components/WhyUsTile";
 const WhyUs = () => {
     return(
         <section className="whyUs">
-            <h1 className="section-header">Why Should You Choose Us?</h1>
+            <h1 className="section-header marker">Why Choose Us?</h1>
             <div className="whyUsContent">
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil iste vero tenetur autem. Sequi, iusto sunt provident aperiam voluptatum at.</p>
                 <div className="whyUsTilesContainer">
