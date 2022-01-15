47ffe83f041891516b98e32f356a2e69e5460e21
Prabodh Panda <46858748+Prabodh-Panda@users.noreply.github.com>
2022-01-15 15:55:49 +0530

    Finished Hero Section

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,15 @@
+import './style.css'
+
+import Navbar from "../../components/Navbar"
+import HeroSection from "../../sections/Home/HeroSection";
+
+const Home = () => {
+    return(
+        <>
+            <Navbar />
+            <HeroSection />
+        </>
+    )
+}
+
+export default Home;
---
+++ src/sections/Home/HeroSection.jsx
@@ -0,0 +1,23 @@
+import animation from '../../drawables/animation.gif'
+
+const HeroSection = () => {
+    return(
+        <div class="main-container">
+            <main>
+                <div class="main-content" data-aos="fade-right">
+                    <h1><span class="accent-text">Creativity <span class="accentBlack">and</span> Imagination</span> are the world's greatest weapons<br/>on this planet</h1>
+                    <div class="desc">
+                        <p>We have them in abundance</p>
+                        <p class="redp">Have us to grow your business</p>
+                    </div>
+                    <a href="" class="link-button">Explore Our Expertise</a>
+                </div>
+                <div class="hero-img" data-aos="fade-up">
+                    <img src={animation} alt="Creativity"/>
+                </div>
+            </main>
+        </div>
+    )
+}
+
+export default HeroSection;
