8db09a38d50739ca04d2b8fa7d0a9a1882eab0b3
Prabodh Panda <46858748+Prabodh-Panda@users.noreply.github.com>
2022-01-17 20:47:45 +0530

    Made changes to footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,10 +1,12 @@
+import './style.css'
+
 const Footer = () => {
     return(
         <section className="Footer">
-            <h1 className="section-header">DZYNFOX</h1>
-            <p>A Brand Of Markall</p>
-            <p>COPYRIGHT &copy; ABMS</p>
-            <p>Images designed with the help of Freepik</p>
+            <h1 className="section-header">DZYN<span className='accentText'>FOX</span></h1>
+            <p>A Brand of Markall</p>
+            <p>COPYRIGHT &copy; Aspire Business Multiplication Solution</p>
+            <p className='attribution'>Images designed with the help of <a href="https://www.freepik.com" target="_blank" rel="nofollow">Freepik</a></p>
         </section>
     )
 }
