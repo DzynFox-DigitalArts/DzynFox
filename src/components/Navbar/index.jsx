8d7a4524cda1002513986668c4ca0774bd8e154b
Prabodh Panda <46858748+Prabodh-Panda@users.noreply.github.com>
2022-01-17 20:12:23 +0530

    Finished Link Colour

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -7,10 +7,10 @@ const Navbar = () => {
             <nav>
                 <a href="/"><img src={logo} alt="DZYNFOX" className="logo" /></a>
                 <ul className="nav-list">
-                    <li className="nav-list-item"><a href="">Home</a></li>
+                    <li className="nav-list-item"><a href="/">Home</a></li>
                     <li className="nav-list-item"><a href="">Our Expertise</a></li>
                     <li className="nav-list-item"><a href="">Our Blog</a></li>
-                    <li className="nav-list-item"><a href="">About Us</a></li>
+                    <li className="nav-list-item"><a href="/about-us">About Us</a></li>
                     <li className="nav-list-item"><a href="">Contact Us</a></li>
                 </ul>
             </nav>
