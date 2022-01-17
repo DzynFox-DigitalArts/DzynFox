9ab06eb090164a1a8e82fe4baffebfe5ced09fe9
Prabodh Panda <46858748+Prabodh-Panda@users.noreply.github.com>
2022-01-17 19:09:53 +0530

    Added Routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
 import Home from './pages/Home';
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import AboutUs from './pages/AboutUs';
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />}/>
+          <Route path="about-us" element={<AboutUs />}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
