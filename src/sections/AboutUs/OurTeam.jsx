fef046de33436cbf8ffaf6084246c3ff51e75936
Prabodh Panda <46858748+Prabodh-Panda@users.noreply.github.com>
2022-01-18 17:21:44 +0530

    Added LI links and Finished IMG Positioning

--- src/sections/AboutUs/OurTeam.jsx
+++ src/sections/AboutUs/OurTeam.jsx
@@ -11,11 +11,11 @@ const OurTeam = () => {
         <section className="OurTeam">
             <h1 className="section-header marker">Meet Our Creative Team</h1>
             <div className="members">
-                <TeamMember imgUrl={Ria} designation={"Head Graphics"} name={"Ria"}/>
+                <TeamMember imgUrl={Ria} designation={"Head Graphics"} name={"Ria"} li={`https://www.linkedin.com/in/ria-roy-9133561aa/`}/>
                 <TeamMember imgUrl={Amrit} designation={"Head Animations"} name={"Amrit"}/>
-                <TeamMember imgUrl={Ruchika} designation={"Graphic Designer"} name={"Ruchika"}/>
+                <TeamMember imgUrl={Ruchika} designation={"Graphic Designer"} name={"Ruchika"} li={`https://www.linkedin.com/in/ruchika-choudhary-587769225/`}/>
                 <TeamMember imgUrl={Ujjwal} designation={"Graphic Designer"} name={"Ujjwal"}/>
-                <TeamMember imgUrl={Prabodh} designation={"Web Developer"} name={"Prabodh"}/>
+                <TeamMember imgUrl={Prabodh} designation={"Web Developer"} name={"Prabodh"} li={`https://www.linkedin.com/in/prabodhpanda/`}/>
             </div>
         </section>
     )
