e00ce673955d52cc4b2e7631c35351589c82d36b
Prabodh Panda <46858748+Prabodh-Panda@users.noreply.github.com>
2022-01-18 17:59:10 +0530

    Changed About Us

--- src/sections/AboutUs/HeroSection.jsx
+++ src/sections/AboutUs/HeroSection.jsx
@@ -4,13 +4,12 @@ const HeroSection = () => {
     return(
         <main className="AboutUsHeroMain">
             <h1 className="section-header">We are <span className="accent-text">DzynFox</span></h1>
-            <p className="section-subheader subheader-main">We are designers.</p>
-            <p className="section-subheader">We think. We plan. We create.<br />We conceive new ideas and bring them to reality.<br />We are creative artists but design things for a purpose</p>
+            <p className="section-subheader">We are designers. <br />We think. We plan. We create.<br />We conceive new ideas and bring them to reality.<br />We are creative artists but design things for a purpose</p>
             <div className="AboutUsHeroMainContent">
                 <img src={AboutPNG} alt="Men Discussing Idea" />
                 <div className="AboutUsHeroContent">
                     <p>
-                    We are DzynFox Digital Arts LLP. A company formed under the MarkAll group of companies. We are the design hands of <span className='aboutus-subheader'>MarkAll - Marketing All Out</span>, a full-service marketing agency.
+                    We are <span className='aboutus-subheader'>DzynFox Digital Arts LLP</span>. A company formed under the MarkAll group of companies. We are the design hands of <span className='aboutus-subheader'>MarkAll - Marketing All Out</span>, a full-service marketing agency.
                     <span className="spacer"></span>
                         <span className="aboutus-subheader">Our mission</span> is to offer creative services and imaginative solutions to strengthen every creative opportunity. We cultivate both the business and its clients through successful visual solutions and do so with professionalism, enthusiasm and dedication.
                     <span className="spacer"></span> 
