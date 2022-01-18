0c9ab39f545c15e1153ee78e940314e72d84ec72
Prabodh Panda <46858748+Prabodh-Panda@users.noreply.github.com>
2022-01-18 20:26:00 +0530

    Changed WhyUsTile in Homepage

--- src/sections/Home/WhyUs.jsx
+++ src/sections/Home/WhyUs.jsx
@@ -7,10 +7,10 @@ const WhyUs = () => {
             <div className="whyUsContent">
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil iste vero tenetur autem. Sequi, iusto sunt provident aperiam voluptatum at.</p>
                 <div className="whyUsTilesContainer">
-                    <WhyUsTile />
-                    <WhyUsTile />
-                    <WhyUsTile />
-                    <WhyUsTile />
+                <WhyUsTile faIconName={'fas fa-bezier-curve'} name={"Best in Class Design"} desc={"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquam, numquam. Facere optio laborum maiores saepe quasi quos, quo rem voluptatibus!"}/>
+                <WhyUsTile faIconName={'fas fa-bezier-curve'} name={"Best in Class Design"} desc={"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquam, numquam. Facere optio laborum maiores saepe quasi quos, quo rem voluptatibus!"}/>
+                <WhyUsTile faIconName={'fas fa-bezier-curve'} name={"Best in Class Design"} desc={"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquam, numquam. Facere optio laborum maiores saepe quasi quos, quo rem voluptatibus!"}/>
+                <WhyUsTile faIconName={'fas fa-bezier-curve'} name={"Best in Class Design"} desc={"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquam, numquam. Facere optio laborum maiores saepe quasi quos, quo rem voluptatibus!"}/>
                 </div>
             </div>
         </section>
