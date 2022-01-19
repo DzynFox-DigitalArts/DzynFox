7536d17080d0dab829b7c26b62c94be70e0a8601
Prabodh Panda <46858748+Prabodh-Panda@users.noreply.github.com>
2022-01-19 18:00:11 +0530

    Added Sinjan Sir to Our Creative Team

--- src/sections/AboutUs/OurTeam.jsx
+++ src/sections/AboutUs/OurTeam.jsx
@@ -1,4 +1,5 @@
 import TeamMember from "../../components/TeamMember"
+import Sinjan from '../../drawables/team/Sinjan.jpg'
 import Ria from '../../drawables/team/Ria.jpg'
 import Amrit from '../../drawables/team/Amrit.jpg'
 import Ruchika from '../../drawables/team/Ruchika.jpg'
@@ -11,11 +12,12 @@ const OurTeam = () => {
         <section className="OurTeam">
             <h1 className="section-header marker">Meet Our Creative Team</h1>
             <div className="members">
-                <TeamMember imgUrl={Ria} designation={"Head Graphics"} name={"Ria"} li={`https://www.linkedin.com/in/ria-roy-9133561aa/`}/>
+                <TeamMember imgUrl={Sinjan} designation={"Founder"} name={"Sinjan"} />
+                <TeamMember imgUrl={Ria} designation={"Head Graphics"} name={"Ria"} />
                 <TeamMember imgUrl={Amrit} designation={"Head Animations"} name={"Amrit"}/>
-                <TeamMember imgUrl={Ruchika} designation={"Graphic Designer"} name={"Ruchika"} li={`https://www.linkedin.com/in/ruchika-choudhary-587769225/`}/>
-                <TeamMember imgUrl={Ujjwal} designation={"Graphic Designer"} name={"Ujjwal"} li={`https://www.linkedin.com/in/ujjwal-kumar-gupta-19a033205/`}/>
-                <TeamMember imgUrl={Prabodh} designation={"Web Developer"} name={"Prabodh"} li={`https://www.linkedin.com/in/prabodhpanda/`}/>
+                <TeamMember imgUrl={Ruchika} designation={"Graphic Designer"} name={"Ruchika"}/>
+                <TeamMember imgUrl={Ujjwal} designation={"Graphic Designer"} name={"Ujjwal"}/>
+                <TeamMember imgUrl={Prabodh} designation={"Web Developer"} name={"Prabodh"}/>
             </div>
         </section>
     )
