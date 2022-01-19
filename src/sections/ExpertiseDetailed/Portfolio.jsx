6e6b3366661f89a30a66b1b1109857a09353f82e
Prabodh Panda <46858748+Prabodh-Panda@users.noreply.github.com>
2022-01-19 18:46:49 +0530

    Added Portfolio Section

--- src/sections/ExpertiseDetailed/Portfolio.jsx
+++ src/sections/ExpertiseDetailed/Portfolio.jsx
@@ -1,9 +1,62 @@
-const Portfolio = ({name}) => {
+import Logo from '../../drawables/goglobal.png'
+
+import Masonry, { ResponsiveMasonry } from "react-responsive-masonry";
+import { useEffect, useState} from "react";
+// import sanityClient from '../../Sanity/client'
+// import imageUrlBuilder from "@sanity/image-url";
+// import ClipLoader from 'react-spinners/ClipLoader';
+
+// const builder = imageUrlBuilder(sanityClient);
+// function urlFor(source) {
+//   return builder.image(source);
+// }
+
+const OurPortfolio = ({name}) => {
+
+    // const [allImage, setAllImages] = useState([]);
+    // const [loading, setLoading] = useState(true);
+
+    // useEffect(() => {
+    //     sanityClient
+    //       .fetch(
+    //         `*[_type == "portfolios" && name == $name][0]{
+    //         name,
+    //         images
+    //     }`, {name}
+    //       )
+    //       .then((data) => {
+    //           setAllImages(data.images);
+    //           setLoading(false);
+    //         })
+    //       .catch(console.error);
+    // }, []);
+
     return(
-        <section className="Portfolio">
-            <h1 className="section-header">Have a look at our {name} portfolio.</h1>
-        </section>
+        <main className="OurPortfolio" id="portfolio">
+            <header>
+                <h1 className="section-header">Our {name} Portfolio</h1>
+                <p className='section-subheader'>Take a look at what our highly capable teams ace at doing</p>
+            </header>
+            <div className="portfolio">
+                {/* <ClipLoader loading={loading} color="#ffffff" /> */}
+                <ResponsiveMasonry
+                    columnsCountBreakPoints={{350: 1, 750: 2, 900: 3}}
+                >
+                    <Masonry gutter="3rem">
+                        <img src={Logo} alt="Client" />
+                        <img src={Logo} alt="Client" />
+                        <img src={Logo} alt="Client" />
+                        <img src={Logo} alt="Client" />
+                        <img src={Logo} alt="Client" />
+                        <img src={Logo} alt="Client" />
+                        <img src={Logo} alt="Client" />
+                        <img src={Logo} alt="Client" />
+                        <img src={Logo} alt="Client" />
+                    </Masonry>
+                </ResponsiveMasonry>
+            </div>
+        </main>
     )
 }
 
-export default Portfolio
+export default OurPortfolio;
