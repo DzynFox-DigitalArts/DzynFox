93555fe49f8f79c01f32075860905bbd8bf3ff36
Prabodh Panda <46858748+Prabodh-Panda@users.noreply.github.com>
2022-01-19 17:48:06 +0530

    Added Videos Section with Language Switch

--- src/sections/Home/WhyUs.jsx
+++ src/sections/Home/WhyUs.jsx
@@ -1,16 +1,49 @@
-import WhyUsTile from "../../components/WhyUsTile";
+import Select from 'react-select';
+import YoutubeEmbed from '../../components/YoutubeEmbed';
+
+import {useState} from 'react'
+
+const selectOptions = [
+    {value: 'hindi', label: 'Hindi'},
+    {value: 'english', label: 'English'},
+]
 
 const WhyUs = () => {
+    
+    const [selectedLanguage, setSelectedLanguage] = useState('english')
+
     return(
         <section className="whyUs">
             <h1 className="section-header marker">Why Choose Us?</h1>
             <div className="whyUsContent">
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil iste vero tenetur autem. Sequi, iusto sunt provident aperiam voluptatum at.</p>
+                <div className="CustomSelectContainer">
+                    <label htmlFor="lang">Select Your Preferred Language: </label>
+                    <Select
+                        className="customSelect"
+                        isSearchable={false} 
+                        options={selectOptions}
+                        defaultValue={{value: 'english', label: 'English'}}
+                        onChange={e => setSelectedLanguage(e.value)}
+                    />
+                </div>
                 <div className="whyUsTilesContainer">
-                <WhyUsTile faIconName={'fas fa-bezier-curve'} name={"Best in Class Design"} desc={"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquam, numquam. Facere optio laborum maiores saepe quasi quos, quo rem voluptatibus!"}/>
-                <WhyUsTile faIconName={'fas fa-bezier-curve'} name={"Best in Class Design"} desc={"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquam, numquam. Facere optio laborum maiores saepe quasi quos, quo rem voluptatibus!"}/>
-                <WhyUsTile faIconName={'fas fa-bezier-curve'} name={"Best in Class Design"} desc={"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquam, numquam. Facere optio laborum maiores saepe quasi quos, quo rem voluptatibus!"}/>
-                <WhyUsTile faIconName={'fas fa-bezier-curve'} name={"Best in Class Design"} desc={"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquam, numquam. Facere optio laborum maiores saepe quasi quos, quo rem voluptatibus!"}/>
+                    {
+                        selectedLanguage === 'english' ? 
+                        <>
+                            <YoutubeEmbed embedId="gsnqXt7d1mU" />
+                            <YoutubeEmbed embedId="gsnqXt7d1mU" />
+                            <YoutubeEmbed embedId="gsnqXt7d1mU" />
+                            <YoutubeEmbed embedId="gsnqXt7d1mU" />
+                        </>
+                        :
+                        <>
+                            <YoutubeEmbed embedId="6stlCkUDG_s" />
+                            <YoutubeEmbed embedId="6stlCkUDG_s" />
+                            <YoutubeEmbed embedId="6stlCkUDG_s" />
+                            <YoutubeEmbed embedId="6stlCkUDG_s" />
+                        </>
+                    }
                 </div>
             </div>
         </section>
