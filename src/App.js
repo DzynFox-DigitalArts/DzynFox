7ff62b6dd1d808bbaef8ae9695cb41ccde309f89
Prabodh Panda <46858748+Prabodh-Panda@users.noreply.github.com>
2022-01-23 20:40:05 +0530

    Made our expertise dynamic

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import './responsive.css';
 import Home from './pages/Home';
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import AboutUs from './pages/AboutUs';
