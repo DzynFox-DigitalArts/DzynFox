a82efd8f7041f73f0b2594cec466a22d46fc8113
Prabodh Panda <46858748+Prabodh-Panda@users.noreply.github.com>
2022-01-23 18:20:03 +0530

    Added state to refresh ifram once the video has finished playing

--- src/components/YoutubeEmbed/index.jsx
+++ src/components/YoutubeEmbed/index.jsx
@@ -1,10 +1,17 @@
 import React from "react";
 import './style.css';
+import YouTube from 'react-youtube';
+import { useState } from "react";
 
-const YoutubeEmbed = ({ embedId }) => (
-  <div className="embedContainer">
+const YoutubeEmbed = ({ embedId }) => {
+
+  //DO NOT REMOVE THIS RANDOM STATE: This state is used to refresh the iframe once the video has ended 
+  const [random, setRandom] = useState();
+
+  return(
+    <div className="embedContainer">
     <div className="video-responsive">
-    <iframe
+    {/* <iframe
       width="853"
       height="480"
       src={`https://www.youtube.com/embed/${embedId}`}
@@ -12,9 +19,15 @@ const YoutubeEmbed = ({ embedId }) => (
       allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
       allowFullScreen
       title="Embedded youtube"
+    /> */}
+    <YouTube 
+      key={random}
+      videoId={embedId}
+      onEnd={event =>  setRandom(Math.random())}
     />
     </div>
   </div>
-);
+  )
+};
 
 export default YoutubeEmbed;
