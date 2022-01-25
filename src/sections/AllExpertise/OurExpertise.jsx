b90403d293a01d4631d2e55bf9c66664ae2d51c7
Prabodh Panda <46858748+Prabodh-Panda@users.noreply.github.com>
2022-01-25 14:27:31 +0530

    Changed all expertise page

--- src/sections/AllExpertise/OurExpertise.jsx
+++ src/sections/AllExpertise/OurExpertise.jsx
@@ -7,7 +7,18 @@ const OurExpertise = () => {
             <h2 className="section-subheader">For all of your designing needs</h2>
             <p className="section-details">From designing your logo to all your branding essentials, everything at one place</p>
             <div className="expertise-container">
-                <ExpertiseTile name={"Logo Design"} faIconName={"fas fa-blog"} />
+
+                <ExpertiseTile name={"Graphic Design"} faIconName={"fas fa-swatchbook"} imgUrl={"https://image.freepik.com/free-vector/graphic-designer-workplace_52683-5306.jpg"} />
+                <ExpertiseTile name={"Logo Design"} faIconName={"fas fa-blog"} imgUrl="https://image.freepik.com/free-vector/logo-design-company-slogan-creating-corporate-branding-identity-graphic-designer-flat-character-researching-competitive-logotype-idea_335657-190.jpg" />
+                <ExpertiseTile name={"Social Media Ads"} faIconName={"fas fa-users"} />
+                <ExpertiseTile name={"Web Design"} faIconName={"fas fa-laptop-code"} />
+
+                <ExpertiseTile name={"Banner/Poster Design"} faIconName={"fas fa-image"} />
+                <ExpertiseTile name={"Advertisement Design"} faIconName={"fas fa-ad"} imgUrl={"https://img.freepik.com/free-vector/design-team-concept-illustration_114360-4531.jpg?size=626&ext=jpg"}/>
+                <ExpertiseTile name={"Promotional Campaign Design"} faIconName={"fas fa-sign"} />
+                <ExpertiseTile name={"Outdoor Ads"} faIconName={"fas fa-torii-gate"} />
+
+                {/* <ExpertiseTile name={"Logo Design"} faIconName={"fas fa-blog"} />
                 <ExpertiseTile name={"Graphic Design"} faIconName={"fas fa-swatchbook"} />
                 <ExpertiseTile name={"Advertisement Design"} faIconName={"fas fa-ad"} />
                 <ExpertiseTile name={"Creative Writing"} faIconName={"fas fa-paragraph"} />
@@ -26,7 +37,7 @@ const OurExpertise = () => {
                 <ExpertiseTile name={"Branding"} faIconName={"far fa-lightbulb"} />
                 <ExpertiseTile name={"Outdoor Ads"} faIconName={"fas fa-torii-gate"} />
                 <ExpertiseTile name={"Banner/Poster Design"} faIconName={"fas fa-image"} />
-                <ExpertiseTile name={"Social Media Ads"} faIconName={"fas fa-users"} />
+                <ExpertiseTile name={"Social Media Ads"} faIconName={"fas fa-users"} /> */}
             </div>
         </section>
     )
