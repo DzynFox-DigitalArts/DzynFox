ebdfc4710370f9b2c762efec73a9c968c778a126
Prabodh Panda <46858748+Prabodh-Panda@users.noreply.github.com>
2022-01-25 21:50:09 +0530

    Implemented Send Grid

--- .netlify/functions/notify.js
+++ .netlify/functions/notify.js
@@ -8,18 +8,6 @@ exports.handler = async (event, context) => {
         'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE'
       };
 
-    // const req = JSON.parse(event.body)
-    // const email = req.email
-    // const msgContent = req.message
-
-    // sgMail.setApiKey('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')
-    // const msg = {
-    //     to: email, // Change to your recipient
-    //     from: 'graphyyy.markall@gmail.com', // Change to your verified sender
-    //     subject: 'New Query!',
-    //     text: msgContent,
-    // }
-
     if (event.httpMethod === "OPTIONS") {
         return {
           statusCode: 200,
@@ -27,23 +15,30 @@ exports.handler = async (event, context) => {
           body: JSON.stringify({ message: "Successful preflight call." }),
         };
       } else if (event.httpMethod === "POST") {
-        return {
-          statusCode: 200,
-          headers,
-          body: event.body,
-        };
-      }
 
-    // try{
-    //     const response = await sgMail.send(msg)
-    //     return {
-    //         statusCode: 200,
-    //         body: "Mail Sent"
-    //     }
-    // } catch (error) {
-    //     return {
-    //         statusCode: 500,
-    //         body: JSON.stringify(error)
-    //     }
-    // }
+        const req = JSON.parse(event.body)
+        const email = req.email
+        const msgContent = req.message
+
+        sgMail.setApiKey('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')
+        const msg = {
+          to: email, // Change to your recipient
+          from: 'graphyyy.markall@gmail.com', // Change to your verified sender
+          subject: 'New Query!',
+          text: msgContent,
+        }
+
+        try{
+          const response = await sgMail.send(msg)
+          return {
+              statusCode: 200,
+              body: "Mail Sent"
+          }
+        } catch (error) {
+          return {
+              statusCode: 500,
+              body: JSON.stringify(error)
+          }
+        }
+      }
 }
