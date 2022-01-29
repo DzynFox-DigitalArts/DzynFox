37c16d5c0467cf41b4930b04159d4715bcc54034
Prabodh Panda <46858748+Prabodh-Panda@users.noreply.github.com>
2022-01-29 17:54:03 +0530

    Made Testimonial Section Dynamic

--- src/sections/AllExpertise/OurExpertise.jsx
+++ src/sections/AllExpertise/OurExpertise.jsx
@@ -13,7 +13,7 @@ const OurExpertise = () => {
                 <ExpertiseTile name={"Social Media Ads"} faIconName={"fas fa-users"} />
                 <ExpertiseTile name={"Web Design"} faIconName={"fas fa-laptop-code"} />
 
-                <ExpertiseTile name={"Banner/Poster Design"} faIconName={"fas fa-image"} />
+                <ExpertiseTile name={"Banner / Poster Design"} faIconName={"fas fa-image"} />
                 <ExpertiseTile name={"Advertisement Design"} faIconName={"fas fa-ad"} />
                 <ExpertiseTile name={"Promotional Campaign Design"} faIconName={"fas fa-sign"} />
                 <ExpertiseTile name={"Outdoor Ads"} faIconName={"fas fa-torii-gate"} />
@@ -21,14 +21,14 @@ const OurExpertise = () => {
                 <ExpertiseTile name={"Branding"} faIconName={"fas fa-blog"} />
                 <ExpertiseTile name={"T-Shirts, Cup, Mug, Pens Design"} faIconName={"fas fa-tshirt"} />
                 <ExpertiseTile name={"Package, Labels and Sticker Design"} faIconName={"fas fa-receipt"} />
-                <ExpertiseTile name={"Leaflet/Handout Design"} faIconName={"fas fa-file-alt"} />
+                <ExpertiseTile name={"Leaflet / Handout Design"} faIconName={"fas fa-file-alt"} />
 
                 <ExpertiseTile name={"Illustration"} faIconName={"fas fa-pen-nib"} />
                 <ExpertiseTile name={"Photo Editing"} faIconName={"fas fa-images"} />
                 <ExpertiseTile name={"Creative Writing"} faIconName={"fas fa-paragraph"} />
-                <ExpertiseTile name={"Newsletters/Magazines Design"} faIconName={"far fa-newspaper"} />
+                <ExpertiseTile name={"Newsletters / Magazines Design"} faIconName={"far fa-newspaper"} />
 
-                <ExpertiseTile name={"Brochure/Catalogue Design"} faIconName={"fas fa-file"} />
+                <ExpertiseTile name={"Brochure / Catalogue Design"} faIconName={"fas fa-file"} />
                 <ExpertiseTile name={"Presentations"} faIconName={"fas fa-tv"} />
                 <ExpertiseTile name={"Animation"} faIconName={"fas fa-walking"} />
                 <ExpertiseTile name={"Video Presentation"} faIconName={"fas fa-video"} />
--- src/sections/Home/StatsSection.jsx
+++ src/sections/Home/StatsSection.jsx
@@ -3,9 +3,11 @@ import CountUpStat from "../../components/CountUpStat"
 const StatsSection = () => {
     return(
         <section className="statsSection">
-            <CountUpStat name={"Years of experience"} suffix={"+"} count={10} faIconName={"far fa-hourglass"}/>
-            <CountUpStat name={"Projects handled"} count={240} faIconName={"fas fa-project-diagram"}/>
-            <CountUpStat name={"Happy Customers"} count={120} faIconName={"far fa-smile-beam"}/>
+            <div className="statsSectionStatsContainer">
+                <CountUpStat name={"Years of experience"} suffix={"+"} count={10} faIconName={"far fa-hourglass"}/>
+                <CountUpStat name={"Projects handled"} count={240} faIconName={"fas fa-project-diagram"}/>
+                <CountUpStat name={"Happy Customers"} count={120} faIconName={"far fa-smile-beam"}/>
+            </div>
         </section>
     )
 }
