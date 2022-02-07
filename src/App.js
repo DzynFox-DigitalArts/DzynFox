ff33bceaf036e4b817e89bd9ee0db5e76e3b3678
Prabodh Panda <46858748+Prabodh-Panda@users.noreply.github.com>
2022-02-07 11:37:53 +0530

    REVERT: LazyLoading

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import './App.css';
 import './responsive.css';
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
-import React from 'react';
 import Home from './pages/Home';
-const AboutUs = React.lazy(() => import('./pages/AboutUs'));
-const AllExpertise = React.lazy(() => import('./pages/AllExpertise'));
-const ExpertiseDetailed = React.lazy(() => import('./pages/ExpertiseDetailed'));
-const Blogs = React.lazy(() => import('./pages/Blogs'));
-const BlogDetailed = React.lazy(() => import('./pages/BlogDetailed'));
+import AboutUs from './pages/AboutUs';
+import AllExpertise from './pages/AllExpertise';
+import ExpertiseDetailed from './pages/ExpertiseDetailed';
+import Blogs from './pages/Blogs';
+import BlogDetailed from './pages/BlogDetailed';
 
 function App() {
 
@@ -16,31 +15,11 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />}/>
-          <Route path="about-us" element={
-            <React.Suspense fallback="Loading...">
-              <AboutUs />
-            </React.Suspense>
-          } />
-          <Route path="expertise" element={
-            <React.Suspense fallback="Loading...">
-              <AllExpertise />
-            </React.Suspense>
-          } />
-          <Route path="expertise/:name" element={
-            <React.Suspense fallback="Loading...">
-              <ExpertiseDetailed />
-            </React.Suspense>
-          } />
-          <Route path="blog" element={
-            <React.Suspense fallback="Loading...">
-              <Blogs />
-            </React.Suspense>
-          } />
-          <Route path="blog/:slug" element={
-            <React.Suspense fallback="Loading...">
-              <BlogDetailed />
-            </React.Suspense>
-          } />
+          <Route path="about-us" element={<AboutUs />}/>
+          <Route path="expertise" element={<AllExpertise />}/>
+          <Route path="expertise/:name" element={<ExpertiseDetailed />}/>
+          <Route path="blog" element={<Blogs />}/>
+          <Route path="blog/:slug" element={<BlogDetailed />}/>
         </Routes>
       </BrowserRouter>
     </div>
