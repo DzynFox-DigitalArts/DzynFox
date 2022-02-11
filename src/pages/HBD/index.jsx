d29658562f70b6c00c587c87058cc7ce1d791f31
Prabodh Panda <46858748+Prabodh-Panda@users.noreply.github.com>
2022-02-11 21:24:04 +0530

    Added HBD Animation

--- src/pages/HBD/index.jsx
+++ src/pages/HBD/index.jsx
@@ -1,6 +1,12 @@
-import { useState } from 'react'
+import { useState, useRef, useEffect } from 'react'
 import Confetti from 'react-confetti'
 import './style.css'
+import Flags from '../../drawables/separated/Flags.png'
+import fast from '../../drawables/separated/fast.png'
+import slow from '../../drawables/separated/slow.png'
+import Cake from '../../drawables/separated/Cake.png'
+import { gsap } from 'gsap'
+
 
 const wishes = [
 `The more you praise and celebrate your life,
@@ -26,22 +32,62 @@ Celebrate being happy every day.`,
 
 const HBD = () => {
 
+    const coverRef = useRef(null)
+    const selector = gsap.utils.selector(coverRef);
+    const tl = useRef();
     const [index, setIndex] = useState(0);
 
     const showNextMsg = () => {
         setIndex((index + 1) % wishes.length)
     }
 
+    useEffect(() => {
+        tl.current = gsap.timeline({defaults: {ease: 'power1.out'}})
+        .to(selector('.fast'), {
+            y: '-200%',
+            duration: 3,
+            delay: 1,
+        })
+    }, [])
+
+    useEffect(() => {
+        tl.current = gsap.timeline({defaults: {ease: 'power1.out'}})
+        .to(selector('.slow'), {
+            y: '-200%',
+            duration: 4,
+            delay: 1,
+        })
+    })
+
+    useEffect(() => {
+        tl.current = gsap.timeline({defaults: {ease: 'power1.out'}})
+        .to(selector('.text'), {
+            y: '0%',
+            duration: 1,
+            delay: 0.5
+        })
+    }, [])
+
     return(
-        <div className="hbd">
+        <div className="hbd" ref={coverRef}>
+            <img src={Flags} alt="Flags" className='hbdBgImageFlag'/>
+            <img src={fast} alt="Flags" className='hbdBgImage fast'/>
+            <img src={slow} alt="Flags" className='hbdBgImage slow'/>
             <Confetti 
-                height={300}
-                width={300}
+                height={window.innerHeight}
+                width={window.innerWidth}
             />
-            <pre>{wishes[index]}</pre>
+            <div className="wish-container">
+                <img src={Cake} alt="Flags" className='hbdBgImageCake'/>
+                <div className="intro-text">
+                    <h1 className="hide">
+                        <span className="text"><pre>{wishes[index]}</pre></span>
+                    </h1>
+                </div>
             <div className="next-wish">
                 <p>We have more wishes for you</p>
                 <button onClick={showNextMsg}>See Next</button>
+            </div>
             </div>
         </div>
     )
