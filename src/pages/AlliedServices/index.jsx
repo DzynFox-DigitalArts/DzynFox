1539d602c9f8da6c71f5861148171685f01455c2
Prabodh Panda <46858748+Prabodh-Panda@users.noreply.github.com>
2022-02-18 18:23:20 +0530

    Finalised Desktop view of allied service page

--- src/pages/AlliedServices/index.jsx
+++ src/pages/AlliedServices/index.jsx
@@ -1,10 +1,15 @@
 import './style.css'
 import AlliedServiceTile from "../../components/AlliedServiceTile";
+import MarkallLogo from '../../drawables/OtherBrands/markallLogo.png';
+import SpreadInIndiaLogo from '../../drawables/OtherBrands/spreadInIndiaLogo.png';
 
 const AlliedServices = () => {
     return(
-        <>
+        <div className="allied-services-container">
             <h1 className="section-header">Allied Services</h1>
+            <h2 className="section-subheader">Alone we can do too little. Together we can do so much.</h2>
+            <p className="section-details">To serve you better we have partnered with few agencies and professional companies for services that are not our specialization, but are important elements of the business.</p>
+            <p className="section-details">The purpose is to have value additions and improved services. Here are few of them</p>
             <div className="allied-services">
                 <AlliedServiceTile faIconName={"fas fa-camera"} name="Photography" desc="Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate iusto est, architecto nam aut ad praesentium aliquam eius neque aliquid!"/>
                 <AlliedServiceTile faIconName={"fas fa-video"} name="Videography" desc="Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate iusto est, architecto nam aut ad praesentium aliquam eius neque aliquid!"/>
@@ -12,7 +17,14 @@ const AlliedServices = () => {
                 <AlliedServiceTile faIconName={"fas fa-trademark"} name="Trademark Registration" desc="Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate iusto est, architecto nam aut ad praesentium aliquam eius neque aliquid!"/>
                 <AlliedServiceTile faIconName={"fas fa-globe-asia"} name="Domain Registration" desc="Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate iusto est, architecto nam aut ad praesentium aliquam eius neque aliquid!"/>
             </div>
-        </>
+            <div className="allied-section-other-brands">
+                <h2 className="section-subheader">You might need services of our other partner companies too.</h2>
+                <div className="brandImages">
+                    <a href="http://www.markall.in/" target="_blank"><img src={MarkallLogo} alt="MarkAll - Marketing All Out" /></a>
+                    <a href="https://spreadinindia.markall.in/" target="_blank"><img src={SpreadInIndiaLogo} alt="Spread In India" /></a>
+                </div>
+            </div>
+        </div>
     )
 }
 
