ed3401776167d39c19156deab42eb5e34ce770c2
Prabodh Panda <46858748+Prabodh-Panda@users.noreply.github.com>
2022-02-18 17:06:20 +0530

    Added Link to Expertise Section

--- src/sections/Home/OurExpertise.jsx
+++ src/sections/Home/OurExpertise.jsx
@@ -2,7 +2,7 @@ import ExpertiseTile from "../../components/ExpertiseTile"
 
 const OurExpertise = () => {
     return(
-        <section className="ourExpertise" id="ExpertiseSection">
+        <section className="ourExpertise" id="expertise">
             <h1 className="section-header">A One-Stop-Design-Studio</h1>
             <h2 className="section-subheader">For all of your designing needs</h2>
             <p className="section-details">From designing your logo to all your branding essentials, everything at one place</p>
