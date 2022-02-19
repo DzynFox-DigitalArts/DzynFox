efb682d2d1fb60d8d12042ca96a53100f486a447
Prabodh Panda <46858748+Prabodh-Panda@users.noreply.github.com>
2022-02-19 21:35:47 +0530

    Fixed: Nav not closing on link press

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,7 +1,7 @@
 import './style.css'
 import logo from '../../drawables/logo.png'
 import {useMediaQuery} from 'react-responsive'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import {Link} from 'react-router-dom'
 import { HashLink } from 'react-router-hash-link'
 
@@ -12,6 +12,11 @@ const Navbar = () => {
     })
 
     const [isNavOpen, setIsNavOpen] = useState(false);
+
+    useEffect(() => {
+        const mobileNavLinks = document.querySelectorAll('.mobile-nav-list .nav-list-item');
+        console.log(mobileNavLinks)
+    }, [])
 
     return(
         <header>
@@ -39,7 +44,7 @@ const Navbar = () => {
             {
                 !isDesktopOrLaptop && isNavOpen && 
                 <div className='mobile-nav'>
-                <ul className="nav-list mobile-nav-list">
+                <ul className="nav-list mobile-nav-list" onClick={() => setIsNavOpen(false)}>
                     <li className="nav-list-item"><Link to="/">Home</Link></li>
                     <li className="nav-list-item"><HashLink to="/#expertise">Services</HashLink></li>
                     <li className="nav-list-item"><Link to="/allied-services">Allied Services</Link></li>
