1e2ee471923f13eb61b1a0d5d2889b468c651e10
Prabodh Panda <46858748+Prabodh-Panda@users.noreply.github.com>
2022-02-19 21:16:33 +0530

    Made Enquiry Form Dynamic

--- src/components/EnquiryForm/index.jsx
+++ src/components/EnquiryForm/index.jsx
@@ -119,7 +119,7 @@ const EnquiryForm = () => {
                 draggable
                 pauseOnHover
             />
-            <div className="quickResponseForm">
+            <div className="quickResponseForm enquiryForm">
                 <h1 className="section-header">Contact Us</h1>
                 <div className="ContactFormContent">
                     <div className="ContactInfo">
