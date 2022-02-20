5268dda3195b93e9427e4abe65b101dc44096046
Prabodh Panda <46858748+Prabodh-Panda@users.noreply.github.com>
2022-02-20 17:42:55 +0530

    Added Team Members

--- src/sections/AboutUs/OurTeam.jsx
+++ src/sections/AboutUs/OurTeam.jsx
@@ -5,6 +5,8 @@ import Amrit from '../../drawables/team/Amrit.jpg'
 import Ruchika from '../../drawables/team/Ruchika.jpg'
 import Ujjwal from '../../drawables/team/Ujjwal.jpg'
 import Prabodh from '../../drawables/team/Prabodh.jpg'
+import Shruti from '../../drawables/team/Shruti.jpg'
+import Mahasweta from '../../drawables/team/Mahasweta.jpg'
 
 
 const OurTeam = () => {
@@ -18,6 +20,8 @@ const OurTeam = () => {
                 <TeamMember imgUrl={Ruchika} designation={"Graphic Designer"} name={"Ruchika"}/>
                 <TeamMember imgUrl={Ujjwal} designation={"Graphic Designer"} name={"Ujjwal"}/>
                 <TeamMember imgUrl={Prabodh} designation={"Web Developer"} name={"Prabodh"}/>
+                <TeamMember imgUrl={Shruti} designation={"Voice Over Artist"} name={"Shruti"}/>
+                <TeamMember imgUrl={Mahasweta} designation={"Illustrator"} name={"Mahasweta"}/>
             </div>
         </section>
     )
